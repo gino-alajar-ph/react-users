eb653355c32bb6c8105dfa66530b4d5dd1218361
Gino Alajar <gino.alajar.ph@gmail.com>
2021-02-27 18:43:02 +0800

    Initial commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './card.styles.css'
+
+export const Card = props => (
+    <div className='card-container'>
+        <img alt="hero" src={`https://robohash.org/${props.hero.id}?set=set5&size=180x180`} />
+        <h2> {props.hero.name} </h2>
+        <p> {props.hero.email} </p>
+    </div>
+   
+);
